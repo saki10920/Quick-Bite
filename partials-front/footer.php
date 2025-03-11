8ed0bc8ea8d96e33c9d46e1fe65a4fed8863e5b2
MD MASUDUR RAHMAN <106664420+saki10920@users.noreply.github.com>
2025-03-11 14:52:12 +0600

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,28 @@
+    <!-- social Section Starts Here -->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/masudurrahman10920"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/this_is_sakii/"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://x.com/this_is_sakii"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>2024 All rights reserved. Designed By <a href="https://www.aust.edu">AUST</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
